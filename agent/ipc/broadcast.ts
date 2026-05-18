959de76162fbc0540c8f937a296693c2a154baeb
mkoch <matthias.koch@smail.inf.h-brs.de>
2026-05-18 13:21:13 +0200

    agents/ipc/* updated with safe usage patterns and centralized error handling

--- agent/ipc/broadcast.ts
+++ agent/ipc/broadcast.ts
@@ -1,6 +1,7 @@
 import { log, devlog, am_send } from "../utils/logging.js"
 import { Where } from "../utils/misc.js"
 import { Java } from "../utils/javalib.js"
+import { safePerform, safeUse, safeOverload } from "../utils/safe_java.js"
 
 const PROFILE_HOOKING_TYPE: string = "IPC_BROADCAST"
 
@@ -24,9 +25,9 @@ based on the work of https://github.com/dpnishant/appmon/blob/master/scripts/And
 */
 
 function hook_broadcasts() {
-    Java.perform(() => {
-        try {
-            const ContextWrapper = Java.use('android.content.ContextWrapper');
+    safePerform("broadcast:hook_broadcasts", () => {
+    const ContextWrapper = safeUse('android.content.ContextWrapper', "broadcast:hook_broadcasts");
+    if (!ContextWrapper) return;
 
             const getIntentInfo = (intent: any) => {
                 const intentData: any = {};
@@ -63,125 +64,155 @@ function hook_broadcasts() {
             };
 
             if (ContextWrapper.sendBroadcast) {
-                ContextWrapper.sendBroadcast.overload('android.content.Intent').implementation = function (intent: any) {
-                    const intentInfo = getIntentInfo(intent);
-                    
-                    createBroadcastEvent("broadcast.sent", {
-                        class: 'android.content.ContextWrapper',
-                        method: 'sendBroadcast',
-                        intent: intentInfo,
-                        stack_trace: getStackTrace()
-                    });
-                    
-                    return this.sendBroadcast.overload('android.content.Intent').apply(this, arguments);
-                };
+                const sendBroadcast1 = safeOverload(
+                    ContextWrapper.sendBroadcast,
+                    "broadcast:ContextWrapper.sendBroadcast",
+                    'android.content.Intent'
+                );
+                if (sendBroadcast1) {
+                    sendBroadcast1.implementation = function(intent: any) {
+                        const intentInfo = getIntentInfo(intent);
+                        createBroadcastEvent("broadcast.sent", {
+                            class: 'android.content.ContextWrapper',
+                            method: 'sendBroadcast',
+                            intent: intentInfo,
+                            stack_trace: getStackTrace()
+                        });
+                        return this.sendBroadcast.overload('android.content.Intent').apply(this, arguments);
+                    };
+                }
 
-                ContextWrapper.sendBroadcast.overload('android.content.Intent', 'java.lang.String').implementation = function (intent: any, receiverPermission: string) {
-                    const intentInfo = getIntentInfo(intent);
-                    
-                    createBroadcastEvent("broadcast.sent", {
-                        class: 'android.content.ContextWrapper',
-                        method: 'sendBroadcast',
-                        intent: intentInfo,
-                        receiver_permission: receiverPermission,
-                        stack_trace: getStackTrace()
-                    });
-                    
-                    return this.sendBroadcast.overload('android.content.Intent', 'java.lang.String').apply(this, arguments);
-                };
+                const sendBroadcast2 = safeOverload(
+                    ContextWrapper.sendBroadcast,
+                    "broadcast:ContextWrapper.sendBroadcast",
+                    'android.content.Intent', 'java.lang.String'
+                );
+                if (sendBroadcast2) {
+                    sendBroadcast2.implementation = function(intent: any, receiverPermission: string) {
+                        const intentInfo = getIntentInfo(intent);
+                        createBroadcastEvent("broadcast.sent", {
+                            class: 'android.content.ContextWrapper',
+                            method: 'sendBroadcast',
+                            intent: intentInfo,
+                            receiver_permission: receiverPermission,
+                            stack_trace: getStackTrace()
+                        });
+                        return this.sendBroadcast.overload('android.content.Intent', 'java.lang.String').apply(this, arguments);
+                    };
+                }
             }
 
             if (ContextWrapper.sendStickyBroadcast) {
-                ContextWrapper.sendStickyBroadcast.overload('android.content.Intent').implementation = function (intent: any) {
-                    const intentInfo = getIntentInfo(intent);
-                    
-                    createBroadcastEvent("broadcast.sticky_sent", {
-                        class: 'android.content.ContextWrapper',
-                        method: 'sendStickyBroadcast',
-                        intent: intentInfo,
-                        stack_trace: getStackTrace()
-                    });
-                    
-                    return this.sendStickyBroadcast.overload('android.content.Intent').apply(this, arguments);
-                };
+                const sendSticky = safeOverload(
+                    ContextWrapper.sendStickyBroadcast,
+                    "broadcast:ContextWrapper.sendStickyBroadcast",
+                    'android.content.Intent'
+                );
+                if (sendSticky) {
+                    sendSticky.implementation = function(intent: any) {
+                        const intentInfo = getIntentInfo(intent);
+                        createBroadcastEvent("broadcast.sticky_sent", {
+                            class: 'android.content.ContextWrapper',
+                            method: 'sendStickyBroadcast',
+                            intent: intentInfo,
+                            stack_trace: getStackTrace()
+                        });
+                        return this.sendStickyBroadcast.overload('android.content.Intent').apply(this, arguments);
+                    };
+                }
             }
 
             if (ContextWrapper.startActivity) {
-                ContextWrapper.startActivity.overload('android.content.Intent').implementation = function (intent: any) {
-                    const intentInfo = getIntentInfo(intent);
-                    
-                    createBroadcastEvent("activity.started", {
-                        class: 'android.content.ContextWrapper',
-                        method: 'startActivity',
-                        intent: intentInfo,
-                        stack_trace: getStackTrace()
-                    });
-                    
-                    return this.startActivity.overload('android.content.Intent').apply(this, arguments);
-                };
+                const startActivity1 = safeOverload(
+                    ContextWrapper.startActivity,
+                    "broadcast:ContextWrapper.startActivity",
+                    'android.content.Intent'
+                );
+                if (startActivity1) {
+                    startActivity1.implementation = function(intent: any) {
+                        const intentInfo = getIntentInfo(intent);
+                        createBroadcastEvent("activity.started", {
+                            class: 'android.content.ContextWrapper',
+                            method: 'startActivity',
+                            intent: intentInfo,
+                            stack_trace: getStackTrace()
+                        });
+                        return this.startActivity.overload('android.content.Intent').apply(this, arguments);
+                    };
+                }
 
-                ContextWrapper.startActivity.overload('android.content.Intent', 'android.os.Bundle').implementation = function (intent: any, bundle: any) {
-                    const intentInfo = getIntentInfo(intent);
-                    
-                    createBroadcastEvent("activity.started", {
-                        class: 'android.content.ContextWrapper',
-                        method: 'startActivity',
-                        intent: intentInfo,
-                        bundle: bundle ? bundle.toString() : null,
-                        stack_trace: getStackTrace()
-                    });
-                    
-                    return this.startActivity.overload('android.content.Intent', 'android.os.Bundle').apply(this, arguments);
-                };
+                const startActivity2 = safeOverload(
+                        ContextWrapper.startActivity,
+                        "broadcast:ContextWrapper.startActivity",
+                        'android.content.Intent', 'android.os.Bundle'
+                    );
+                if (startActivity2) {
+                    startActivity2.implementation = function(intent: any, bundle: any) {
+                        const intentInfo = getIntentInfo(intent);
+                        createBroadcastEvent("activity.started", {
+                            class: 'android.content.ContextWrapper',
+                            method: 'startActivity',
+                            intent: intentInfo,
+                            bundle: bundle ? bundle.toString() : null,
+                            stack_trace: getStackTrace()
+                        });
+                        return this.startActivity.overload('android.content.Intent', 'android.os.Bundle').apply(this, arguments);
+                    };
+                }
             }
 
             if (ContextWrapper.startService) {
-                ContextWrapper.startService.implementation = function (service: any) {
+                ContextWrapper.startService.implementation = function(service: any) {
                     const intentInfo = getIntentInfo(service);
-                    
                     createBroadcastEvent("service.started", {
                         class: 'android.content.ContextWrapper',
                         method: 'startService',
                         service: intentInfo,
                         stack_trace: getStackTrace()
                     });
-                    
                     return this.startService.apply(this, arguments);
                 };
             }
 
             if (ContextWrapper.stopService) {
-                ContextWrapper.stopService.implementation = function (name: any) {
+                ContextWrapper.stopService.implementation = function(name: any) {
                     const intentInfo = getIntentInfo(name);
-                    
                     createBroadcastEvent("service.stopped", {
                         class: 'android.content.ContextWrapper',
                         method: 'stopService',
                         service: intentInfo,
                         stack_trace: getStackTrace()
                     });
-                    
                     return this.stopService.apply(this, arguments);
                 };
             }
 
             if (ContextWrapper.registerReceiver) {
-                ContextWrapper.registerReceiver.overload('android.content.BroadcastReceiver', 'android.content.IntentFilter').implementation = function (receiver: any, filter: any) {
-                    return this.registerReceiver.apply(this, arguments);
-                };
+                const registerReceiver1 = safeOverload(
+                    ContextWrapper.registerReceiver,
+                    "broadcast:ContextWrapper.registerReceiver",
+                    'android.content.BroadcastReceiver', 'android.content.IntentFilter'
+                );
+                if (registerReceiver1) {
+                    registerReceiver1.implementation = function(receiver: any, filter: any) {
+                        return this.registerReceiver.apply(this, arguments);
+                    };
+                }
 
-                ContextWrapper.registerReceiver.overload('android.content.BroadcastReceiver', 'android.content.IntentFilter', 'java.lang.String', 'android.os.Handler').implementation = function (receiver: any, filter: any, broadcastPermission: string, scheduler: any) {
-                    return this.registerReceiver.apply(this, arguments);
-                };
+                const registerReceiver2 = safeOverload(
+                    ContextWrapper.registerReceiver,
+                    "broadcast:ContextWrapper.registerReceiver",
+                    'android.content.BroadcastReceiver', 'android.content.IntentFilter',
+                    'java.lang.String', 'android.os.Handler'
+                );
+                if (registerReceiver2) {
+                    registerReceiver2.implementation = function(receiver: any, filter: any, broadcastPermission: string, scheduler: any) {
+                        return this.registerReceiver.apply(this, arguments);
+                    };
+                }
             }
-        } catch (error) {
-            createBroadcastEvent("broadcast.error", {
-                error: (error as Error).toString(),
-                stack_trace: getStackTrace()
-            });
-        }
-    });
-}
+        });
+    }
 
 
 export function install_broadcast_hooks(){
--- agent/ipc/shared_prefs.ts
+++ agent/ipc/shared_prefs.ts
@@ -1,6 +1,7 @@
 import { log, devlog, am_send } from "../utils/logging.js"
 import { get_path_from_fd } from "../utils/android_runtime_requests.js"
 import { Java } from "../utils/javalib.js"
+import { safePerform, safeUse, safeOverload } from "../utils/safe_java.js"
 
 const PROFILE_HOOKING_TYPE: string = "IPC_SHARED-PREF"
 
@@ -25,92 +26,112 @@ function createSharedPrefEvent(eventType: string, data: Partial<SharedPrefEvent>
 }
 
 function install_shared_preferences_hooks() {
+    devlog("\n")
     devlog("Installing SharedPreferences hooks");
     
-    Java.perform(() => {
-        const SharedPrefs = Java.use('android.app.SharedPreferencesImpl');
-        const SharedPrefsEditor = Java.use('android.app.SharedPreferencesImpl$EditorImpl');
+    safePerform("shared_prefs:install_shared_preferences_hooks", () => {
+        const SharedPrefs = safeUse('android.app.SharedPreferencesImpl', "shared_prefs:install_shared_preferences_hooks");
+        const SharedPrefsEditor = safeUse('android.app.SharedPreferencesImpl$EditorImpl', "shared_prefs:install_shared_preferences_hooks");
 
         // Hook SharedPreferences initialization
-        SharedPrefs.$init.overload('java.io.File', 'int').implementation = function(file, mode) {
-            const result = this.$init(file, mode);
-            
-            createSharedPrefEvent("shared_prefs.init", {
-                method: "SharedPreferencesImpl.$init",
-                file: file.getAbsolutePath(),
-                mode: mode
-            });
-            
-            return result;
-        };
+        if (SharedPrefs) {
+                const sharedPrefsInit = safeOverload(
+                    SharedPrefs.$init,
+                    "shared_prefs:SharedPreferencesImpl.$init",
+                    'java.io.File', 'int'
+                );
+                if (sharedPrefsInit) {
+                    sharedPrefsInit.implementation = function(file, mode) {
+                        const result = this.$init(file, mode);
+                        createSharedPrefEvent("shared_prefs.init", {
+                            method: "SharedPreferencesImpl.$init",
+                            file: file.getAbsolutePath(),
+                            mode: mode
+                        });
+                        return result;
+                    };
+                }
+            }
 
         // Hook SharedPreferences Editor putString
-        SharedPrefsEditor.putString.overload('java.lang.String', 'java.lang.String').implementation = function(key, value) {
-            createSharedPrefEvent("shared_prefs.put_string", {
-                method: "putString",
-                key: key,
-                value: value
-            });
-            
-            return this.putString(key, value);
-        };
-
-        // Hook other Editor methods
-        const editorMethods = [
-            { method: 'putInt', args: ['java.lang.String', 'int'] },
-            { method: 'putLong', args: ['java.lang.String', 'long'] },
-            { method: 'putFloat', args: ['java.lang.String', 'float'] },
-            { method: 'putBoolean', args: ['java.lang.String', 'boolean'] }
-        ];
-
-        editorMethods.forEach(({ method, args }) => {
-            try {
-                SharedPrefsEditor[method].overload(...args).implementation = function(key: string, value: any) {
-                    createSharedPrefEvent(`shared_prefs.${method.toLowerCase()}`, {
-                        method: method,
+        if (SharedPrefsEditor) {
+            const putString = safeOverload(
+                SharedPrefsEditor.putString,
+                "shared_prefs:SharedPreferencesImpl$EditorImpl.putString",
+                'java.lang.String', 'java.lang.String'
+            );
+            if (putString) {
+                putString.implementation = function(key, value) {
+                    createSharedPrefEvent("shared_prefs.put_string", {
+                        method: "putString",
                         key: key,
-                        value: value.toString()
+                        value: value
                     });
-                    
-                    return this[method](key, value);
+                    return this.putString(key, value);
                 };
-            } catch (e) {
-                devlog(`Could not hook SharedPrefsEditor.${method}: ${e}`);
             }
-        });
 
+            // Hook other Editor methods
+            const editorMethods = [
+                { method: 'putInt',     args: ['java.lang.String', 'int'] },
+                { method: 'putLong',    args: ['java.lang.String', 'long'] },
+                { method: 'putFloat',   args: ['java.lang.String', 'float'] },
+                { method: 'putBoolean', args: ['java.lang.String', 'boolean'] }
+            ];
 
+            editorMethods.forEach(({ method, args }) => {
+                const overload = safeOverload(
+                    SharedPrefsEditor[method],
+                    `shared_prefs:SharedPreferencesImpl$EditorImpl.${method}`,
+                    ...args
+                );
+                if (overload) {
+                    overload.implementation = function(key: string, value: any) {
+                        createSharedPrefEvent(`shared_prefs.${method.toLowerCase()}`, {
+                            method: method,
+                            key: key,
+                            value: value.toString()
+                        });
+                        return this[method](key, value);
+                    };
+                }
+            });
+        }
     });
 }
 
 function install_datastore_hooks() {
+    devlog("\n")
     devlog("Installing DataStore hooks");
     
-    Java.perform(() => {
-        try {
-            // Hook the DataStore class
-            const DataStore = Java.use("androidx.datastore.core.DataStore");
-
+    safePerform("shared_prefs:install_datastore_hooks", () => {
+        // Hook the DataStore class
+        const DataStore = safeUse("androidx.datastore.core.DataStore", "shared_prefs:install_datastore_hooks");
+        if (DataStore) {
             // Hook updateData
-            DataStore.updateData.overload("kotlin.coroutines.Continuation").implementation = function (continuation) {
-                const result = this.updateData(continuation);
-
-                // Log the result if possible
-                result.then((data: any) => {
-                    createSharedPrefEvent("datastore.update", {
-                        method: "updateData",
-                        data: data ? data.toString() : null
+            const updateData = safeOverload(
+                DataStore.updateData,
+                "shared_prefs:DataStore.updateData",
+                "kotlin.coroutines.Continuation"
+            );
+            if (updateData) {
+                updateData.implementation = function(continuation) {
+                    // Log the result if possible
+                    const result = this.updateData(continuation);
+                    result.then((data: any) => {
+                        createSharedPrefEvent("datastore.update", {
+                            method: "updateData",
+                            data: data ? data.toString() : null
+                        });
                     });
-                });
-
-                return result;
-            };
+                    return result;
+                };
+            }
 
             // Hook data (flow)
             if (DataStore.getData) {
-                DataStore.getData.overload().implementation = function () {
+                DataStore.getData.overload().implementation = function() {
                     const flow = this.getData();
-
                     // Hook into the flow to log emitted data
                     flow.collect((data: any) => {
                         createSharedPrefEvent("datastore.get", {
@@ -118,41 +139,50 @@ function install_datastore_hooks() {
                             data: data ? data.toString() : null
                         });
                     });
-
                     return flow;
                 };
             }
-        } catch (e) {
-            devlog(`DataStore not available: ${e}`);
         }
 
-        try {
-            // Hook Preferences DataStore (key-value)
-            const Preferences = Java.use("androidx.datastore.preferences.core.Preferences");
-            Preferences.get.overload("androidx.datastore.preferences.core.Preferences$Key").implementation = function (key) {
-                const value = this.get(key);
-                
-                createSharedPrefEvent("datastore_prefs.get", {
-                    method: "get",
-                    key: key ? key.toString() : "unknown",
-                    value: value ? value.toString() : null
-                });
-                
-                return value;
-            };
+        // Hook Preferences DataStore (key-value)
+        const Preferences = safeUse("androidx.datastore.preferences.core.Preferences", "shared_prefs:install_datastore_hooks");
+        if (Preferences) {
+            const prefsGet = safeOverload(
+                Preferences.get,
+                "shared_prefs:Preferences.get",
+                "androidx.datastore.preferences.core.Preferences$Key"
+            );
 
             // Hook Preferences.Key class
-            const PreferencesKey = Java.use("androidx.datastore.preferences.core.Preferences$Key");
-            PreferencesKey.$init.overload("java.lang.String").implementation = function (keyName) {
-                createSharedPrefEvent("datastore_prefs.key_init", {
-                    method: "$init",
-                    key: keyName
-                });
-                
-                return this.$init(keyName);
-            };
-        } catch (e) {
-            devlog(`Preferences DataStore not available: ${e}`);
+            if (prefsGet) {
+                prefsGet.implementation = function(key) {
+                    const value = this.get(key);
+                    createSharedPrefEvent("datastore_prefs.get", {
+                        method: "get",
+                        key: key ? key.toString() : "unknown",
+                        value: value ? value.toString() : null
+                    });
+                    return value;
+                };
+            }
+        }
+
+        const PreferencesKey = safeUse("androidx.datastore.preferences.core.Preferences$Key", "shared_prefs:install_datastore_hooks");
+        if (PreferencesKey) {
+            const keyInit = safeOverload(
+                PreferencesKey.$init,
+                "shared_prefs:Preferences$Key.$init",
+                "java.lang.String"
+            );
+            if (keyInit) {
+                keyInit.implementation = function(keyName) {
+                    createSharedPrefEvent("datastore_prefs.key_init", {
+                        method: "$init",
+                        key: keyName
+                    });
+                    return this.$init(keyName);
+                };
+            }
         }
     });
 }
